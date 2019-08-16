55f7ae616884f01ae959230bbde5743b0b1625e2
Koljasha <koljasha@mail.ru>
2019-08-16 11:27:04 +0300

    rm deploy branch

--- webpack.config.js
+++ webpack.config.js
@@ -9,92 +9,95 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 // const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-const devMode = process.env.NODE_ENV !== 'production';
+module.exports = (env, options) => {
+    const devMode = options.mode !== 'production';
 
-module.exports = {
-    entry: {
-        index: './src/script.js'
-    },
-    output: {
-        path: path.resolve(__dirname, 'static'),
-        filename: '[name].bundle.js',
-    },
+    const config = {
+        entry: {
+            index: './src/script.js'
+        },
+        output: {
+            path: path.resolve(__dirname, 'static'),
+            filename: '[name].bundle.js',
+        },
 
-    optimization: {
-        minimizer: [
-            new UglifyJsPlugin({}),
-            new OptimizeCSSAssetsPlugin({})
-        ]
-    },
+        optimization: {
+            minimizer: [
+                new UglifyJsPlugin({}),
+                new OptimizeCSSAssetsPlugin({})
+            ]
+        },
 
-    plugins: [
-        new CleanWebpackPlugin(['static', 'templates']),
-        new MiniCssExtractPlugin({}),
-        new HtmlWebpackPlugin({
-            filename: '../templates/index.html',
-            template: './src/index.html',
-            minify: devMode ? false : {
-                removeComments: true,
-                collapseWhitespace: true,
-                conservativeCollapse: true
-            },
-        }),
-        new HtmlWebpackPlugin({
-            filename: '../templates/login.html',
-            template: './src/login.html',
-            minify: devMode ? false : {
-                removeComments: true,
-                collapseWhitespace: true,
-                conservativeCollapse: true
-            },
-            inject: false
-        }),
-        // new VueLoaderPlugin(),
-        // new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
-    ],
+        plugins: [
+            new CleanWebpackPlugin(['static', 'templates']),
+            new MiniCssExtractPlugin({}),
+            new HtmlWebpackPlugin({
+                filename: '../templates/index.html',
+                template: './src/index.html',
+                minify: devMode ? false : {
+                    removeComments: true,
+                    collapseWhitespace: true,
+                    conservativeCollapse: true
+                },
+            }),
+            new HtmlWebpackPlugin({
+                filename: '../templates/login.html',
+                template: './src/login.html',
+                minify: devMode ? false : {
+                    removeComments: true,
+                    collapseWhitespace: true,
+                    conservativeCollapse: true
+                },
+                inject: false
+            }),
+            // new VueLoaderPlugin(),
+            // new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
+        ],
 
-    resolve: {
-        alias: {
-            'vue$': 'vue/dist/vue.esm.js'
-        }
-    },
-
-    module: {
-        rules: [{
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-                    "css-loader"
-                ]
-            },
-            // {
-            //     test: /\.scss$/,
-            //     use: [
-            //         devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-            //         "css-loader",
-            //         'sass-loader'
-            //     ]
-            // },
-            // {
-            //     test: /\.vue$/,
-            //     loader: 'vue-loader'
-            // },
-            {
-                test: /\.(png|jpg|jss)$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]'
-                    }
-                }]
+        resolve: {
+            alias: {
+                'vue$': 'vue/dist/vue.esm.js'
             }
-        ]
-    }
-}
+        },
+
+        module: {
+            rules: [{
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader"
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                        "css-loader"
+                    ]
+                },
+                // {
+                //     test: /\.scss$/,
+                //     use: [
+                //         devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                //         "css-loader",
+                //         'sass-loader'
+                //     ]
+                // },
+                // {
+                //     test: /\.vue$/,
+                //     loader: 'vue-loader'
+                // },
+                {
+                    test: /\.(png|jpg|jss)$/,
+                    use: [{
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]'
+                        }
+                    }]
+                }
+            ]
+        }
+    };
+    return config;
+};
